af5972c47e068cac005e1816f01d08a2e93d4c70
Mike Hannon(code) <mjhannon@gmail.com>
2015-11-01 20:28:20 -0800

    modularize controller_template

--- server.js
+++ server.js
@@ -23,63 +23,7 @@ var user = mongoose.model('User', userSchema);
 /* Load Model */
 var User = mongoose.model('User');
 // Controller
-var genericController = function(model_name, model_string_name, update_array) {
-
-  return {
-    index : function(req, res) {
-      model_name.find({}, function(err, data){
-        if (err){}
-        else {
-          console.log(data);
-          res.sendFile(path.join(__dirname, './public/'+model_string_name+'/index.html'));
-        }
-      })
-    }, //end of index
-
-    create : function(req,res){
-      console.log(req.body);
-      user = new model_name({name:'mike'}); // TEST CODE
-      user.save();
-      res.sendFile(path.join(__dirname, './public/'+model_string_name+'/create.html'));
-    }, // end of create
-
-    show : function(req,res){
-      model_name.findOne({_id:req.params.id}, function (err, user){
-        if (err){console.log(err);}
-        else {
-          console.log(user);
-          res.sendFile(path.join(__dirname, './public/'+model_string_name+'/show.html'));
-        }
-      })
-    }, //end of show
-
-    edit : function(req,res){
-      res.sendFile(path.join(__dirname, './public/'+model_string_name+'/edit.html'));
-    }, // end of edit
-
-    new : function(req,res){
-      res.sendFile(path.join(__dirname, './public/'+model_string_name+'/new.html'));
-    }, // end of new
-
-    update : function(req,res){
-      //define key value pairs that should be updated with this controller
-      var update_object = {};
-      for (var i = 0; i < update_array.length; i ++){
-        update_object[update_array[i]] = req.body[update_array[i]];
-      }
-      //end of define
-      model_name.update({_id:req.params.id}, { $set: update_object}, {upsert: true}, function (err, previous_user) {
-      if (err) {return handleError(err);}
-      console.log(previous_user); // will require a show method or index to update dynamically
-      res.sendFile(path.join(__dirname, './public/'+model_string_name+'/update.html'));
-      });
-    }, //end of update
-
-    delete : function(req,res){
-      res.sendFile(path.join(__dirname, './public/'+model_string_name+'/delete.html'));
-    } //end of delete
-  } // end of return
-};
+var build_controller = require("./server/lib/controller_template.js");
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -99,7 +43,7 @@ var buildRoutes = function(app, controller){
 }
 //end of route builder
 //CREATE CONTROLLLER and routes (example)
-var Users_Controller = new genericController(User, "users", ['name']);
+var Users_Controller = new build_controller(User, "users", ['name']);
 buildRoutes(app, Users_Controller);
 //end
 
---
+++ server/lib/controller_template.js
@@ -0,0 +1,60 @@
+var path = require('path');
+
+module.exports = (function(model_name, model_string_name, update_array) {
+    console.log(__dirname);
+
+  return {
+    index : function(req, res) {
+      model_name.find({}, function(err, data){
+        if (err){}
+        else {
+          console.log(data);
+          res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/index.html'));
+        }
+      })
+    }, //end of index
+
+    create : function(req,res){
+      console.log(req.body);
+      user = new model_name({name:'mike'}); // TEST CODE
+      user.save();
+      res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/create.html'));
+    }, // end of create
+
+    show : function(req,res){
+      model_name.findOne({_id:req.params.id}, function (err, user){
+        if (err){console.log(err);}
+        else {
+          console.log(user);
+          res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/show.html'));
+        }
+      })
+    }, //end of show
+
+    edit : function(req,res){
+      res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/edit.html'));
+    }, // end of edit
+
+    new : function(req,res){
+      res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/new.html'));
+    }, // end of new
+
+    update : function(req,res){
+      //define key value pairs that should be updated with this controller
+      var update_object = {};
+      for (var i = 0; i < update_array.length; i ++){
+        update_object[update_array[i]] = req.body[update_array[i]];
+      }
+      //end of define
+      model_name.update({_id:req.params.id}, { $set: update_object}, {upsert: true}, function (err, previous_user) {
+      if (err) {return handleError(err);}
+      console.log(previous_user); // will require a show method or index to update dynamically
+      res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/update.html'));
+      });
+    }, //end of update
+
+    delete : function(req,res){
+      res.sendFile(path.join(__dirname, './../../public/'+model_string_name+'/delete.html'));
+    } //end of delete
+  } // end of return
+});
